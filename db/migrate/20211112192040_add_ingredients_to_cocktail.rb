dd257edba2a3c83b4afb80176f8283d859c38e70
McKenna Penley <mckennapenley@gmail.com>
2021-11-12 14:24:26 -0500

    adds ingredient fields to cocktail model

--- db/migrate/20211112192040_add_ingredients_to_cocktail.rb
+++ db/migrate/20211112192040_add_ingredients_to_cocktail.rb
@@ -4,6 +4,32 @@ class AddIngredientsToCocktail < ActiveRecord::Migration[6.1]
     add_column :cocktails, :ingredient1, :string
     add_column :cocktails, :measure2, :string
     add_column :cocktails, :ingredient2, :string
+     add_column :cocktails, :measure3, :string
+    add_column :cocktails, :ingredient3, :string
+     add_column :cocktails, :measure4, :string
+    add_column :cocktails, :ingredient4, :string
+     add_column :cocktails, :measure5, :string
+    add_column :cocktails, :ingredient5, :string
+     add_column :cocktails, :measure6, :string
+    add_column :cocktails, :ingredient6, :string
+     add_column :cocktails, :measure7, :string
+    add_column :cocktails, :ingredient7, :string
+     add_column :cocktails, :measure8, :string
+    add_column :cocktails, :ingredient8, :string
+     add_column :cocktails, :measure9, :string
+    add_column :cocktails, :ingredient9, :string
+     add_column :cocktails, :measure10, :string
+    add_column :cocktails, :ingredient10, :string
+     add_column :cocktails, :measure11, :string
+    add_column :cocktails, :ingredient11, :string
+     add_column :cocktails, :measure12, :string
+    add_column :cocktails, :ingredient12, :string
+     add_column :cocktails, :measure13, :string
+    add_column :cocktails, :ingredient13, :string
+     add_column :cocktails, :measure14, :string
+    add_column :cocktails, :ingredient14, :string
+     add_column :cocktails, :measure15, :string
+    add_column :cocktails, :ingredient15, :string
     add_column :cocktails, :instructions, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_213436) do
+ActiveRecord::Schema.define(version: 2021_11_12_192040) do
 
   create_table "cocktails", force: :cascade do |t|
     t.integer "api_id"
@@ -19,6 +19,37 @@ ActiveRecord::Schema.define(version: 2021_11_11_213436) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "measure1"
+    t.string "ingredient1"
+    t.string "measure2"
+    t.string "ingredient2"
+    t.string "measure3"
+    t.string "ingredient3"
+    t.string "measure4"
+    t.string "ingredient4"
+    t.string "measure5"
+    t.string "ingredient5"
+    t.string "measure6"
+    t.string "ingredient6"
+    t.string "measure7"
+    t.string "ingredient7"
+    t.string "measure8"
+    t.string "ingredient8"
+    t.string "measure9"
+    t.string "ingredient9"
+    t.string "measure10"
+    t.string "ingredient10"
+    t.string "measure11"
+    t.string "ingredient11"
+    t.string "measure12"
+    t.string "ingredient12"
+    t.string "measure13"
+    t.string "ingredient13"
+    t.string "measure14"
+    t.string "ingredient14"
+    t.string "measure15"
+    t.string "ingredient15"
+    t.text "instructions"
   end
 
   create_table "favorites", force: :cascade do |t|
