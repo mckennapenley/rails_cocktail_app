6ae1566bc6c9ff507a5a514c805f5d0b510e7a06
McKenna Penley <mckennapenley@gmail.com>
2021-11-14 19:32:53 -0500

    Fix issue in production.js due to copy paste error

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,5 @@
-lsof -wni tcp:3000process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
-const environment = require('./environment')
+const environment = require("./environment");
 
-module.exports = environment.toWebpackConfig()
+module.exports = environment.toWebpackConfig();
